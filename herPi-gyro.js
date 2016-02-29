b604066f842ecb9a2eee3510e1cb13764aa9257a
Andras T. Takacs <att@pulilab.com>
2016-02-29 18:26:09 +0100

    refining deviceorientation to be Android-proof

--- herPi-gyro.js
+++ herPi-gyro.js
@@ -1,71 +1,73 @@
-// INIT
-var x = 90;
-var y = 2;
-var z = 90;
+// Global variables
+var x, y, z;
 
-var alpha, beta, gamma;
+// SOCKET INIT
+var webSocketAddr = 'ws://192.168.0.42:8000/';
+var gyroS = new WebSocket(webSocketAddr);
 
-var gyroS = new WebSocket("ws://192.168.0.42:8000/");
 gyroS.onopen = function (event) {
-	console.info('Connected to WebSocket');
+	document.getElementById('connected').innerHTML = 'Connected to websocket @' + webSocketAddr;
 };
+
 
 window.setTimeout(function() {
 	if (window.DeviceOrientationEvent) {
 		document.getElementById('gotOr').innerHTML = 'Orientation:';
 
-  // 	var doc = window.document;
-  // 	var docEl = doc.documentElement;
 
-  // 	var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+		window.addEventListener("deviceorientation", function(eventData) {
+			// BETA
+			x = ~~eventData.beta;
 
-		// document.body.requestFullscreen();
-		// requestFullscreen.call(docEl);
+			// GAMMA
+			y = ~~eventData.gamma;
+
+			// ALPHA
+			z = ~eventData.alpha;
+
+		});
+
+		window.setTimeout(interval, 1000);
+
+
+
+	} else {
+		console.warn('No deviceorientation on device!!');
 	}
 }, 0);
 
-window.addEventListener("deviceorientation", function(eventData) {		
-	// BETA
-	beta = eventData.beta;
+function interval() {
+	window.setInterval(function() {
 
-	x = ~~beta - 90;
-	if (x < 0) { x = x + 360 }
+		var xx = (y > 0) ? -(y-90) :
+						(y > -45) ? 90 :
+						0;
 
-	// GAMMA
-	gamma = eventData.gamma;
+		var yy = ((x > 45 && x < 135) ? 45 :
+										(x < -136) ? - x - 180 :
+										(x < -45) ? -45 :
+										(x >= 135) ? -(x-180) :
+										x) + 45;
 
-	y = Math.abs((-(~~gamma) + 90) - 180);
-	y = y <= 0 ? 2 : y;
+		var zz = Math.abs(((x >= -90 && x<= 90) ? // If looking downwards...
+													((z >= -90) ? z + 90 :
+													 	(z <= -270) ? z + 450 :
+													 	(z >= -180) ? 0 :
+													 	180) :
+											(z >= -90) ? 180 : // Looking upwards
+											(z <= -270) ? 0 :
+											(z + 270)) - 180);
 
-	// ALPHA
-	alpha = ~~eventData.alpha;
+			gyroS.send( JSON.stringify(
+				{
+					x: (yy || 1).toFixed(2),
+					y: (xx || 1).toFixed(2),
+					z: (zz || 1).toFixed(2)
+				}
+			));
 
-	if (gamma < 0) { // up
-		z = 90;
-	} else { //down
-		z = Math.abs(alpha -360);
-	}
-	// z = alpha;
-	// z = Math.abs(alpha - 360);
-	// z = (Math.floor(~~alpha/2)) + 90;
-	//z = Math.abs(~~alpha - 360);
-	//z = (z + 90)%360;
-	//z = z > 270 ? 0 : z > 180 ? 180 : z;
-	// z = (~~alpha + 90);
-	//z = z > 180 ? 180 : z;
-	// z = z <= 0 ? 2 : z;
-
-});
-
-window.setInterval(function() {
-	gyroS.send(JSON.stringify(
-		{
-			x: x,
-			y: y,
-			z: 90 //TODO: fix z
-		}
-	));
-	document.getElementById('roll').innerHTML = x;
-	document.getElementById('pitch').innerHTML = y;
-	document.getElementById('yaw').innerHTML = z;
-}, 20);
+		document.getElementById('roll').innerHTML = yy;
+		document.getElementById('pitch').innerHTML = xx;
+		document.getElementById('yaw').innerHTML = zz;
+	}, 80);
+}
